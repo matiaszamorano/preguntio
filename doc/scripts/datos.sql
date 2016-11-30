155728b2356e6c0c073532d0c6cb01ce6105fd34
uva <uva@CT15PC014>
2016-11-30 15:53:53 -0300

    Datos de prueba

---
+++ doc/scripts/datos.sql
@@ -0,0 +1,9 @@
+DELETE FROM pregunta;
+
+
+INSERT INTO pregunta
+    (id, contenido)
+VALUES
+       (1, '¿Qué expectativas tenés para el año que viene?'),
+       (2, '¿En qué cosa nueva querés invertir tu tiempo?'),
+       (3, '¿Qué le contas a otros de tu lugar de trabajo?');
