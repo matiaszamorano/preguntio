94123b27987ce3357c2c9b762190ba8cef97dec5
uva <uva@CT15PC014>
2016-11-30 15:31:18 -0300

    Commit inicial

---
+++ src/main/java/com/preguntio/PreguntioApplication.java
@@ -0,0 +1,12 @@
+package com.preguntio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreguntioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreguntioApplication.class, args);
+	}
+}
