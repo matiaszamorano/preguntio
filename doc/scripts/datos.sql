e1c1a3282cad8544302242f5cb532ef7c46affc8
uva <uva@CT15PC014>
2016-12-12 16:43:43 -0300

    Avance parcial con spring security

--- doc/scripts/datos.sql
+++ doc/scripts/datos.sql
@@ -1,4 +1,6 @@
 DELETE FROM pregunta;
+DELETE FROM rol;
+DELETE FROM usuario;
 
 
 INSERT INTO pregunta
@@ -7,3 +9,13 @@ VALUES
        (1, '¿Qué expectativas tenés para el año que viene?'),
        (2, '¿En qué cosa nueva querés invertir tu tiempo?'),
        (3, '¿Qué le contas a otros de tu lugar de trabajo?');
+
+INSERT INTO usuario
+    (id,    email,                      password)
+VALUES
+    (1,     'matzam@gmail.com',         '$2a$10$iodzSOMStstDQmgofPOXGOywxiLcDHtztfNmZ0DS9h5tpb.p3vOiG');
+
+INSERT INTO rol
+    (id_usuario,    rol)
+VALUES
+    (1,             'ROLE_USER');
