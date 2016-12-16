e932ee07a45b01646ab89b2c85d4301df8d4cd10
uva <uva@CT15PC014>
2016-12-16 16:29:54 -0300

    Heroku keep alive

--- src/main/java/com/preguntio/PreguntioApplication.java
+++ src/main/java/com/preguntio/PreguntioApplication.java
@@ -1,12 +1,32 @@
 package com.preguntio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class PreguntioApplication {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(PreguntioApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Scheduled(cron = "0 0/20 7-23 * * *")
+    public void herokuKeepAlive() {
+        System.out.println("Control de ejecución " + System.currentTimeMillis());
+        restTemplate.getForObject("https://preguntio.herokuapp.com/", String.class);
     }
 }
