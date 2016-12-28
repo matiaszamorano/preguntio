43d2d65ecf14ff03efe630f43763ead8877913bc
uva <uva@CT15PC014>
2016-12-28 17:38:41 -0300

    Agrego la posibilidad de ordenar en los servicios

--- src/main/java/com/preguntio/repository/ColeccionRepository.java
+++ src/main/java/com/preguntio/repository/ColeccionRepository.java
@@ -1,10 +1,10 @@
 package com.preguntio.repository;
 
 import com.preguntio.domain.Coleccion;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "colecciones", path = "colecciones")
-public interface ColeccionRepository extends CrudRepository<Coleccion, Long> {
+public interface ColeccionRepository extends PagingAndSortingRepository<Coleccion, Long> {
 
 }
